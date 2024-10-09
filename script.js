809695ec34066e6b5af5914d3bb5280c7ded8efb
Makoder <Mlopez141601@gmail.com>
2024-10-09 16:54:47 +0700

    Added navbar on all pages, fixed responsivity for all elements on home page, added comments on CSS file

---
+++ script.js
@@ -0,0 +1,8 @@
+// All - Navbar
+
+const toggleButton = document.getElementsByClassName('toggler')[0];
+const navLinks = document.getElementsByClassName('navbar-links');
+toggleButton.addEventListener('click', function() {
+    for(var i=0; i<navLinks.length; i++)
+        navLinks[i].classList.toggle('switch')
+});
